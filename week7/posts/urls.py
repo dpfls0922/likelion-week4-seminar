29a931489b2a0ee074371c94d66202111ccfa86b
dpfls0922 <dpfls0922@naver.com>
2024-06-30 23:56:12 +0900

    add: 라우터 추가

--- week7/posts/urls.py
+++ week7/posts/urls.py
@@ -1,12 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 
 from .views import post_create_view, post_delete_view
 from .views import post_list_view, post_update_view, post_detail_view
 from .views import post_form_view, post_create_form_view
+from .views import PostModelViewSet
+from rest_framework import routers
 
 app_name='posts'
 
+router = routers.DefaultRouter()
+router.register('posts', PostModelViewSet)
+
 urlpatterns=[
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('', post_list_view, name='post-list'),
     #path('new/', post_create_view),
     path('form/', post_form_view, name='post-form'),
